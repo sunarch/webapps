9955d0053f1a0a0fbd269c3e79df07af51dc5017
András Németh <sunarch@protonmail.com>
2022-08-19 10:13:48 +0200

    Add algorithm comments

--- docs/distribution/pure.js
+++ docs/distribution/pure.js
@@ -5,9 +5,12 @@
 // algorithms
 
 function algo_units(parts, cutoff) {
+    // Do not include units if there are no parts at all 
     if (parts == 0) {
         return 0;
     }
+
+    // Include one page if any small-sized parts
     if (parts < 1) {
         return 1;
     }
@@ -15,9 +18,12 @@ function algo_units(parts, cutoff) {
     const parts_whole = Math.floor(parts);
     const parts_decimals = parts % 1;
     
+    // If the decimal part is below the cutoff treshold,
+    // do not include an extra page for it
     if (parts_decimals <= cutoff) {
         return parts_whole;
     }
     
+    // By default, include an extra page for decimal part
     return parts_whole + 1;
 }
